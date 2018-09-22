b2d81f395659d32b79bb81945be3fd907729e917
santosh <santosh.sunny12@gmail.com>
2018-09-21 20:04:14 -0400

    routing

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var express     = require("express"),
 	app 		= express(),
 	mongoose	= require("mongoose");
 
+app.use("/", function(req, res){
+	res.render("home");
+});
+
+app.use("/login", function(req, res){
+	res.render("login");
+});
+
+app.use("/register", function(req, res){
+	res.render("register");
+});
 
 app.listen(8080, function(){
 	console.log("PORT = "+process.env.PORT);
